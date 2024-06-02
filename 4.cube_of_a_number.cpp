e82682bc6778f25e09fb4a52d843dfb803bb1f1b
Aryan <149221850+aryan-kundu@users.noreply.github.com>
2024-06-03 01:03:43 +0530

    Add files via upload

---
+++ 4.cube_of_a_number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int x;cin>>x;
+cout << "number is "<< x<<endl ;
+cout <<"cube of the number is "<< x*x*x << endl;
+return 0;
+}
---
+++ 5.size_of_basic_datatypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "\nSize of fundamental data types :\n";
+cout << "The size of char datatype is : " << sizeof(char) << " bytes \n" ;
+cout << "The size of short datatype is : " << sizeof(short) << " bytes \n" ;
+cout << "The size of int datatype is : " << sizeof(int) << " bytes \n" ;
+cout << "The size of long datatype is: " << sizeof(long) << " bytes \n" ;
+cout << "The size of long long datatype is :" << sizeof(long long) << " bytes \n";
+cout << "The size of float datatype is :" << sizeof(float) << " bytes \n" ;
+cout << "The size of double datatype is :" << sizeof(double) << " bytes \n";
+cout << "The size of long double datatype is :" << sizeof(long double) << " bytes \n";
+cout << "The size of bool datatype is : " << sizeof(bool) << " bytes \n\n";
+return 0;
+}
